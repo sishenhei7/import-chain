c4c12d460c02852b826c2a7cc4012af17fd533f0
sishenhei7 <982505136@qq.com>
2023-03-10 09:37:32 +0800

    feat: add directory structure

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-console.log(111111111111)
-export const a = 2
+import ts from 'typescript'
+
+class FileItem {
+  constructor(
+    public path: string,
+    private imports: Set<string> = new Set(),
+    private parents: Set<string> = new Set()
+  ) {}
+
+  public addImport(path: string) {
+    this.imports.add(path)
+  }
+
+  public addParents(path: string) {
+    this.parents.add(path)
+  }
+}
+
+type FileStore = Map<string, FileItem>
+
+export default function getImportChain (filePath: string): string[][] {
+
+  return []
+}
